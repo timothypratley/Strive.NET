f58771bb86105783d5011b64d480a02ccd883552
nathan_rogers <nathan_rogers>
2002-11-26 06:47:58 +0000

    *** empty log message ***

--- Application Source/Strive/Utils/Shared/API.cs
+++ Application Source/Strive/Utils/Shared/API.cs
@@ -11,7 +11,6 @@ namespace Strive.Utils
 {
 	public class API
 	{
-
 		public static XmlDocument GetStoredProcedures(ArrayList storedProcedures)
 		{
 			
@@ -88,6 +87,127 @@ namespace Strive.Utils
 
 		}
 
+		public static string GenerateSchemaFromTables(SQLDMO.Tables tables, string ConnectionString)
+		{
+			SqlConnection con = new SqlConnection(ConnectionString);
+
+			con.Open();
+
+			DataSet schema = new DataSet("Schema");
+
+			foreach(Table enumTable in tables)
+			{
+				if(!enumTable.SystemObject)
+				{
+					string selectList = "SELECT ";
+			
+					// try to build sql string
+					foreach(Column c in enumTable.Columns)
+					{
+						selectList += c.Name + ", ";
+					}
+					if(selectList.EndsWith(", "))
+					{
+						selectList = selectList.Substring(0, selectList.Length -2);
+					}
+
+					selectList += " FROM " + enumTable.Name + " WHERE 1 = 2";
+
+					SqlCommand com = new SqlCommand(selectList, con);
+
+					SqlDataAdapter da = new SqlDataAdapter(selectList, con);
+					da.FillSchema(schema, SchemaType.Source, enumTable.Name);
+				}
+
+			}
+			
+			foreach(Table enumTable in tables)
+			{
+				if(!enumTable.SystemObject)
+				{
+					foreach(Key enumKey in enumTable.Keys)
+					{
+						if(enumKey.Type == SQLDMO.SQLDMO_KEY_TYPE.SQLDMOKey_Foreign)
+						{
+							// add keys
+							string[] childColumnNames;
+							string[] parentColumnNames;
+							string parentTableName;
+							string childTableName;
+						
+							parentTableName = enumKey.ReferencedTable;
+							parentTableName = parentTableName.Replace("[", "");
+							parentTableName = parentTableName.Replace("]", "");
+							parentTableName = parentTableName.Replace("dbo.", "");
+							childTableName = enumTable.Name;
+
+							SQLDMO.Names refColNames = enumKey.ReferencedColumns;
+							ArrayList aryref = new ArrayList();
+							foreach(string refs in refColNames)
+							{
+								aryref.Add(refs);
+							}
+							parentColumnNames = (string[])aryref.ToArray(typeof(string));
+
+							SQLDMO.Names chiColNames = enumKey.KeyColumns;
+							ArrayList arychi = new ArrayList();
+							foreach(string chis in chiColNames)
+							{
+								arychi.Add(chis);
+							}
+							childColumnNames = (string[])arychi.ToArray(typeof(string));
+
+							DataTable ParentTable = schema.Tables[parentTableName];
+							DataTable ChildTable = schema.Tables[childTableName];
+
+							DataColumn[] ParentColumns;
+							ArrayList aryParentColumns = new ArrayList();
+							foreach(string enumParentColumnName in parentColumnNames)
+							{
+								if(ParentTable.Columns[enumParentColumnName] != null)
+								{
+									aryParentColumns.Add(ParentTable.Columns[enumParentColumnName]);
+								}
+							}
+							ParentColumns = (DataColumn[])aryParentColumns.ToArray(typeof(DataColumn));
+						
+							DataColumn[] ChildColumns;
+							ArrayList aryChildColumns = new ArrayList();
+							foreach(string enumChildColumnName in childColumnNames)
+							{
+								if(ParentTable.Columns[enumChildColumnName] != null)
+								{
+									aryChildColumns.Add(ChildTable.Columns[enumChildColumnName]);
+								}
+							}
+							ChildColumns = (DataColumn[])aryChildColumns.ToArray(typeof(DataColumn));
+
+							if(ParentColumns.Length <= 0 ||
+								ChildColumns.Length <= 0)
+							{
+								//System.Windows.Forms.MessageBox.Show("Could not enable '" + enumKey.Name + "' between '" + ParentTable.TableName + "' and '" + ChildTable.TableName + "'.");	
+							}
+							else
+							{
+								DataRelation dr = new DataRelation(enumKey.Name, ParentColumns, ChildColumns, true);
+								schema.Relations.Add(dr);
+							}
+
+						
+						}
+					}
+				}
+			}
+
+
+
+			con.Close();
+
+			return schema.GetXmlSchema();
+
+		}
+		
+
 		private static void AddTablesToSchema(SqlConnection con, DataSet schema, SQLDMO.Table t)
 		{
 			string selectList = "SELECT ";
@@ -109,7 +229,7 @@ namespace Strive.Utils
 			SqlDataAdapter da = new SqlDataAdapter(selectList, con);
 			da.FillSchema(schema, SchemaType.Source, t.Name);
 
-			SQLDMO.QueryResults qt = t.EnumReferencingTables(true);
+			SQLDMO.QueryResults qt = t.EnumReferencedTables(false);
 
 			for(int row = 1; row < qt.Rows; row++)
 			{
@@ -123,15 +243,34 @@ namespace Strive.Utils
 					(!schema.Tables.Contains(tablename)) )
 				{
 					AddTablesToSchema(con, schema, enumTable);
+					SQLDMO.QueryResults qk = t.EnumReferencingKeys(enumTable.Name, false);
+					for(int keyrow = 1; keyrow < qk.Rows; keyrow++)
+					{
+						string keyname = qk.GetColumnString(row, 1);
+						keyname = keyname.Replace("[", "");
+						keyname = keyname.Replace("]", "");
+						keyname = keyname.Replace("dbo.", "");
+						Key k = locateKey(t, keyname);
+
+						if(schema.Relations == null ||
+							(!schema.Relations.Contains(keyname)))
+						{
+							// add keys
+							string[] childColumnNames;
+							string[] parentColumnNames;
+							string parentTableName;
+							string childTableName;
+							
+						}
+					}
 				}
-					
 
 			}
 
 
 		}
 
-		private static Table locateTable(SQLDMO.Database d, string name)
+		public static Table locateTable(SQLDMO.Database d, string name)
 		{
 
 			foreach(Table t in d.Tables)
@@ -144,6 +283,18 @@ namespace Strive.Utils
 			throw new Exception("Could not locate '" + name + "'.");
 		}
 
+		private static Key locateKey(Table t, string name)
+		{
+			foreach(Key k in t.Keys)
+			{
+				if(k.Name == name)
+				{
+					return k;
+				}
+			}
 
+			throw new Exception("COuld not locate '" + name + "'.");
+
+		}
 	}
 }
