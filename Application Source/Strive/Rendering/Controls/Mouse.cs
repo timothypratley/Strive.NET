9e62ce791d121817a6b2c9277f98983092fd5aa5
tpratley <tpratley>
2003-01-21 00:04:56 +0000

    *** empty log message ***

--- Application Source/Strive/Rendering/Controls/Mouse.cs
+++ Application Source/Strive/Rendering/Controls/Mouse.cs
@@ -1,4 +1,5 @@
 using System;
+using Revolution3D8088c;
 
 namespace Strive.Rendering.Controls
 {
@@ -11,7 +12,13 @@ namespace Strive.Rendering.Controls
 		public bool button1down, button2down, button3down, button4down;
 		public static Mouse GetState() {
 			Mouse m = new Mouse();
-			Interop._instance.Control.Mouse_GetState( ref m.x, ref m.y, ref m.button1down, ref m.button2down, ref m.button3down, ref m.button4down, true );
+			R3DMouseState_Type ms = Interop._instance.Control.Mouse_GetState( true );
+			m.x = ms.x;
+			m.y = ms.y;
+			m.button1down = ms.iButton[0] != 0;
+			m.button2down = ms.iButton[1] != 0;
+			m.button3down = ms.iButton[2] != 0;
+			m.button4down = ms.iButton[3] != 0;
 			return m;
 		}
 
