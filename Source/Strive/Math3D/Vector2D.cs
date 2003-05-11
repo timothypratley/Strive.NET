f465054651adba27529ce7babea8e4d4de8912af
tpratley <tpratley>
2003-05-11 11:53:08 +0000

    we need one :)

---
+++ Source/Strive/Math3D/Vector2D.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace Strive.Math3D
+{
+	/// <summary>
+	/// Summary description for Vector2d.
+	/// </summary>
+	public class Vector2D
+	{
+		public float X;
+		public float Y;
+
+		public Vector2D() {
+			this.X = 0;
+			this.Y = 0;
+		}
+
+		public Vector2D( float x, float y ) {
+			X = x;
+			Y = y;
+		}
+	}
+}
