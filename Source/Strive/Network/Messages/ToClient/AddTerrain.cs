f12941ca4c24e5f8c18d076bb6557f92e46639c8
tpratley <tpratley>
2004-05-13 07:02:51 +0000

    *** empty log message ***

--- Source/Strive/Network/Messages/ToClient/AddTerrain.cs
+++ Source/Strive/Network/Messages/ToClient/AddTerrain.cs
@@ -7,11 +7,29 @@ namespace Strive.Network.Messages.ToClient
 	/// <summary>
 	/// Summary description for AddTerrain.
 	/// </summary>
-	public class AddTerrain : AddPhysicalObject {
-		public Terrain terrain;
+
+	public struct TerrainAtom {
+		public float heights;
+		public int textureIDs;
+		public EnumTerrainType terrainType;
+	}
+
+	public class AddTerrain {
+		public int startX, startZ;
+		public int squareSize;
+		public int width, height;
+		public TerrainAtom [][] map;
+
+
 		public AddTerrain(){}
-		public AddTerrain( Terrain t ) {
-			this.terrain = t;
+
+		public AddTerrain( Terrain t, int squareSize, int width, int height ) {
+			this.startX = t.startZ;
+			this.squareSize = squareSize;
+			this.width = width;
+			this.height = height;
+			this.map = new TerrainAtom[width][height];
 		}
 	}
 }
+
