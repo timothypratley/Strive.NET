8c9405460a0efdf0308e3d7a6f2518f47eddd05f
tpratley <tpratley>
2004-05-30 04:20:41 +0000

    building tools

---
+++ Source/Strive/Common/Constants.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Strive.Common
+{
+	/// <summary>
+	/// Summary description for Constants.
+	/// </summary>
+	public class Constants
+	{
+		public static int terrainPieceSize = 10;
+		public static int terrainHeightsPerChunk = 8;
+
+	}
+}
