14eef1b1b5cc9f40f782d8060edaf5b8657a25c3
Timothy Pratley <timothypratley@gmail.com>
2010-08-08 00:26:58 -0700

    wow, a bound listview

---
+++ Source/Strive/Strive.Client/Strive.Client.NeoAxisView/World.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Engine.MathEx;
+using Engine.Renderer;
+using Engine.MapSystem;
+using Engine.EntitySystem;
+using Engine.PhysicsSystem;
+using Engine.Utils;
+using Engine;
+using GameCommon;
+using GameEntities;
+using WindowsAppFramework;
+using System.Windows.Forms;
+
+using Strive.Client.ViewModel;
+
+
+namespace Strive.Client.NeoAxisView
+{
+    public class World
+    {
+        static Form _splash = new Splash();
+        public static WorldViewModel ViewModel;
+        public static bool Init(WorldViewModel worldViewModel)
+        {
+            ViewModel = worldViewModel;
+            // hook up event handlers
+
+            //NeoAxis initialization
+            _splash.Show();
+            if (!WindowsAppWorld.Init(_splash, "user:Logs/WindowsAppExample.log"))
+            {
+                _splash.Close();
+                return false;
+            }
+            _splash.Hide();
+
+            //load map
+            WindowsAppWorld.MapLoad("Maps/RTSDemo/Map.map", true);
+            return true;
+        }
+    }
+}
---
+++ Source/Strive/Strive.Client/Strive.Client.WPF/ResourceList.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using AvalonDock;
+using Strive.Client.ViewModel;
+
+namespace Strive.Client.WPF
+{
+    /// <summary>
+    /// Interaction logic for ResourceList.xaml
+    /// </summary>
+    public partial class ResourceList : DockableContent
+    {
+        public WorldViewModel ViewModel;
+        public ResourceList(WorldViewModel viewModel)
+        {
+            this.ViewModel = viewModel;
+            InitializeComponent();
+            listView1.ItemsSource = ViewModel.ViewEntities;
+            ViewModel.ViewEntities.Add("foo", new ViewEntity("foo", 3, 0, 0, 0, 0, 0, 0));
+        }
+    }
+}
