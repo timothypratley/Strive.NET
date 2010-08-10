336696c4e360cb6f67d403ec139c95f6d9bca8ef
Timothy Pratley <timothypratley@gmail.com>
2010-08-09 19:55:30 -0700

    Wohoo - MVVC server monitor dashboard skeleton

---
+++ Source/Strive/Strive.Server/Strive.Server.WPF/ViewModel/LogViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace Strive.Server.WPF.ViewModel
+{
+    class LogViewModel
+    {
+        public ObservableCollection<string> LogEntries { get; private set; }
+
+        public LogViewModel()
+        {
+            LogEntries = new ObservableCollection<string>();
+            LogEntries.Add("Test");
+            LogEntries.Add("FOo");
+        }
+    }
+}
