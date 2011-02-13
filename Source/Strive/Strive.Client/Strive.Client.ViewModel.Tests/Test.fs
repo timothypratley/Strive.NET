b1e2e39f369688b01fe717b1d39bd6420e546af8
Timothy Pratley <timothypratley@gmail.com>
2011-02-12 20:35:57 -0800

    nunit

---
+++ Source/Strive/Strive.Client/Strive.Client.ViewModel.Tests/Test.fs
@@ -0,0 +1,27 @@
+﻿#light
+
+module Strive.Client.ViewModel.Tests.Test
+
+open System.Net
+open System.Net.Sockets
+open NUnit.Framework
+open FsUnit
+open Strive.Client.ViewModel
+open Strive.Network.Messaging
+
+
+[<TestFixture>]
+type ``my top level text``() = class
+
+    let wvm = new WorldViewModel(null)
+    //let p = new Perspective()
+
+    [<Test>]
+    member self.``some text``() =
+        2 |> should equal 2
+
+    [<Test>]
+    member self.``Can host``() =
+        wvm.AddOrReplace("foo", 
+
+end
